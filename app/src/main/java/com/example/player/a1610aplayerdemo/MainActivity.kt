8cd413f31f9442f336fbc8c076948a145e1c363a
liuqing <2050526054@qq.com>
2019-06-09 19:16:01 +0800

    liuqing

--- app/src/main/java/com/example/player/a1610aplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/player/a1610aplayerdemo/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.player.a1610aplayerdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("aaa","你好");
     }
 }
