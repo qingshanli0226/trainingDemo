21d61703512d76cb525f17a848fcd69e82f7c9e1
liuqing <2050526054@qq.com>
2019-06-10 17:25:10 +0800

    6.10下午引导页完善,欢迎界面，主界面

--- app/src/main/java/com/example/player/a1610aplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/player/a1610aplayerdemo/MainActivity.kt
@@ -1,26 +1,76 @@
 package com.example.player.a1610aplayerdemo
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.view.View
 import android.widget.ImageView
+import com.example.player.a1610aplayerdemo.activity.HomeActivity
 import com.example.player.a1610aplayerdemo.adapter.guides.MyGuidesAdapter
+import com.example.player.a1610aplayerdemo.url.Website
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var guides:ArrayList<View>? = null
     private var myGuidesAdapter:MyGuidesAdapter? = null
+    private var sharedPreferences:SharedPreferences? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initdata()
-        initadpter()
+        sharedPreferences = getSharedPreferences("2019_06_10", Context.MODE_PRIVATE);
+        var ischeck: Boolean? = sharedPreferences?.getBoolean("ischeck",true);
+        if(ischeck!!){
+            initdata()
+            initadpter()
+        }else{
+            main_guide_vp.visibility = View.GONE
+            main_greet_iv.visibility = View.VISIBLE
+            Picasso.with(this@MainActivity).load(Website.GREET).into(main_greet_iv)
+            main_greet_tv.visibility = View.VISIBLE
+            main_greet_tv.setOnClickListener(object:View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    val intent:Intent = Intent(this@MainActivity,HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            })
+        }
     }
 
     private fun initadpter(){
         myGuidesAdapter = MyGuidesAdapter(this,guides);
         main_guide_vp.adapter = myGuidesAdapter
+
+        val guide_vp:ViewPager = findViewById(R.id.main_guide_vp)
+        guide_vp.setOnPageChangeListener(object:ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(p0: Int) {
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+
+            }
+
+            override fun onPageSelected(p0: Int) {
+                if(p0 == guides?.size!!-1){
+                    main_guide_btn.visibility = View.VISIBLE
+                    main_guide_btn.setOnClickListener {
+                        var editor:SharedPreferences.Editor = sharedPreferences!!.edit()
+                        editor.putBoolean("ischeck",false)
+                        editor.commit();
+                        val intent:Intent = Intent(this@MainActivity,HomeActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }else{
+                    main_guide_btn.visibility = View.GONE
+                }
+            }
+        })
     }
 
     private fun initdata(){
--- app/src/main/java/com/example/player/a1610aplayerdemo/url/Website.java
+++ app/src/main/java/com/example/player/a1610aplayerdemo/url/Website.java
@@ -1,5 +1,7 @@
 package com.example.player.a1610aplayerdemo.url;
 
+import com.squareup.picasso.Picasso;
+
 public class Website {
     public static String GREET = "http://ali-files.yooshow.com/2019/03/20/ecccd992-f2a7-4d33-a414-5348c34d28ab.png";
 }
