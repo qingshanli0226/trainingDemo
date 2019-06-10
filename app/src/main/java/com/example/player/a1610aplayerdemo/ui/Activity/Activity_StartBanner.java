80675a232c19d00cdba6095f7038cfb0b7a8a114
lll <304476727@gg.com>
2019-06-10 11:19:07 +0800

    布局

---
+++ app/src/main/java/com/example/player/a1610aplayerdemo/ui/Activity/Activity_StartBanner.java
@@ -0,0 +1,13 @@
+package com.example.player.a1610aplayerdemo.ui.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_StartBanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__start_banner);
+    }
+}
