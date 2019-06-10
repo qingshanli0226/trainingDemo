d8454941217a2235f72198538369a7861e751abd
liqingshan <liqingshan@sina.cn>
2019-06-10 16:03:18 +0800

    for base fragment.

--- app/src/main/java/com/example/player/a1610aplayerdemo/base/BaseRecyclerFragment.java
+++ app/src/main/java/com/example/player/a1610aplayerdemo/base/BaseRecyclerFragment.java
@@ -1,6 +1,40 @@
 package com.example.player.a1610aplayerdemo.base;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class BaseRecyclerFragment<T extends BaseRecyclerViewAdapter> extends Fragment {
+public abstract class BaseRecyclerFragment<T, V extends View> extends Fragment implements IBaseView<T> {
+    private IBasePresenter iBasePresenter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        initView();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private void initView() {
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+        iBasePresenter = getPresenter();
+        iBasePresenter.getData();
+    }
+
+
+    public abstract BaseRecyclerViewAdapter<T, V> getAdapter();
+
+    public abstract IBasePresenter getPresenter();
 }
---
+++ app/src/main/java/com/example/player/a1610aplayerdemo/base/IBasePresenter.java
@@ -0,0 +1,6 @@
+package com.example.player.a1610aplayerdemo.base;
+
+public interface IBasePresenter {
+    void destroyView();
+    void getData();
+}
