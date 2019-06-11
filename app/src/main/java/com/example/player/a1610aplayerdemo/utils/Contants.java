1de3243d2b5b903334e3d4ed24583ced50cdcb2a
lll <304476727@gg.com>
2019-06-11 14:26:19 +0800

    asd

---
+++ app/src/main/java/com/example/player/a1610aplayerdemo/utils/Contants.java
@@ -0,0 +1,27 @@
+package com.example.yangyinonline.util;
+
+/**
+ * Created by wn on 2019/4/17.
+ */
+
+public class Contants {
+    //正式服务器
+    public static final String BASE_URL="http://api.immedc.com/restapi/";
+    //获得匿名登录
+    public static final String CREATE_NEW="account/createNew";
+    //获取课程选择页面数据
+    public static final String GET_HOME="loading/getHome";
+    //获取广告
+    public static final String GET_ADVERT="loading/getStartBanner";
+    //SP的Token名称
+    public static final String TOKEN = "accessToken";
+    //SP的名称
+    public static final String SP = "YangYinOnline";
+    //获取视频
+    public static final String VEDIOURL = "course/getVideo2";
+    //获取音乐课堂分类
+    public static final String CATEGORY = "category/getPerfesional";
+    //获取音乐课堂中的具体某一科目数据
+    public static final String CATEGORY_COURSE = "course/getCourses";
+
+}
