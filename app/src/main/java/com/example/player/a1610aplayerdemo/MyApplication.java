d7bee7a3f291548d9db94dff433e52b8bfd334a5
yangnan <yangnan@qq.com>
2019-06-25 14:14:37 +0800

    集成友盟

--- app/src/main/java/com/example/player/a1610aplayerdemo/MyApplication.java
+++ app/src/main/java/com/example/player/a1610aplayerdemo/MyApplication.java
@@ -2,11 +2,13 @@ package com.example.player.a1610aplayerdemo;
 
 import android.app.Application;
 import com.example.player.a1610aplayerdemo.utils.TokenSp;
+import com.umeng.commonsdk.UMConfigure;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         TokenSp.getInstance().init(this);
+        UMConfigure.init(this,UMConfigure.DEVICE_TYPE_PHONE,null);
     }
 }
