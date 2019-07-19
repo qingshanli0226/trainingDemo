72f18a389b3ebd00c6f3857ac76629bd2ee2f599
liuqing <2050526054@qq.com>
2019-07-19 15:09:53 +0800

    lq7.19

---
+++ app/src/main/java/com/example/player/a1610aplayerdemo/Test.java
@@ -0,0 +1,12 @@
+package com.example.player.a1610aplayerdemo;
+
+public class Test {
+
+    public void getmynum(){
+        runing("你好");
+    }
+
+    private void runing(String num) {
+        
+    }
+}
